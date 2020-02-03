73e7a2e4e5f80448ad409b0a850b6dd5df589e9f
Bossz <deathdreamerlive@gmail.com>
2020-02-03 18:13:10 +0700

    start Shop

---
+++ Tower Defense/Assets/Scripts/Shop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseStandardTurrets()
+    {
+        Debug.Log("PurchaseStandardTurrets Success");
+    }
+    public void PurchaseAnotherTurrets()
+    {
+        Debug.Log("PurchaseAnotherTurrets Success");
+    }
+    public void PurchaseSpecialTurrets()
+    {
+        Debug.Log("PurchaseSpecialTurrets Success");
+    }
+}
